658fc18a259fb4b4e279b8504c74ef611fdca8cc
Junaid Mohamed <junaidmohamed@MacBook-Pro-3.home>
2023-06-20 18:03:29 -0400

    more

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-const header = document.getElementById("header");
+const header = document.getElementById("header")
+
+function submitForm(event) {
+    event.preventDefault();
+
+    var nameInput = document.getElementById('name');
+    var emailInput = document.getElementById('address');
+
+    var name = nameInput.value;
+    var email = emailInput.value;
+
+    var data = {
+      name: name,
+      email: email
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://ki0m5rtgz5.execute-api.us-east-1.amazonaws.com/appendEmail');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        console.log('Request sent successfully!');
+        // You can perform additional actions here after the request is successful
+      } else {
+        console.log('Request failed. Status:', xhr.status);
+        // You can handle the error case here
+      }
+    };
+    xhr.send(JSON.stringify(data));
+  }
+
 window.addEventListener("scroll", function () {
     if (scrollY > 50) {
         header.style.backgroundColor = "rgba(74, 108, 247, .2)"
@@ -8,8 +38,11 @@ window.addEventListener("scroll", function () {
         header.style.backdropFilter = "blur(0px)"
     }
 })
+
 const mood = document.getElementById("change-mood");
+
 const root = document.getElementById("root")
+
 mood.addEventListener("click", function (){
    if ( root.classList.contains("dark")){
        root.classList.remove("dark")
