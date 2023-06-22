752f6876574c0cffa0fe6660e6ce3a00a06b5242
Junaid Mohamed <junaidmohamed@MacBook-Pro-3.home>
2023-06-21 21:07:02 -0400

    more

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.getElementById("header")
 
 function submitTicket(event) {
-  
+    console.log('submit ticket triggered')
     const name = document.getElementById("nameSupport").value;
     const email = document.getElementById("emailSupport").value;
     const message = document.getElementById("message").value;
@@ -29,17 +29,9 @@ function submitTicket(event) {
         });
   }
 
-window.addEventListener("scroll", function () {
-    if (scrollY > 50) {
-        header.style.backgroundColor = "rgba(74, 108, 247, .2)"
-        header.style.backdropFilter = "blur(15px)"
-    }else {
-        header.style.backgroundColor = "transparent"
-        header.style.backdropFilter = "blur(0px)"
-    }
-})
-
 function submitForm(event) {
+    console.log('add email triggered')
+
     const name = document.getElementById("nameSignUp").value;
     const email = document.getElementById("address").value;
     console.log(name,email)
